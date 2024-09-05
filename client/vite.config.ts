f60664eb65dd73103159b50637c84f94ca3835d3
Reményi Mihály Bendegúz <remibende@gmail.com>
2024-09-05 19:00:46 +0200

    deploy prep

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/CV/', // Base path for GitHub Pages
   plugins: [react()],
-})
+});
