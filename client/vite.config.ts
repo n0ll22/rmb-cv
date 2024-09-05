70da6ffa2549f15e1b1387413a95ed904be08c56
Reményi Mihály Bendegúz <remibende@gmail.com>
2024-09-05 19:09:58 +0200

    Typo fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/RMV-CV/', // Base path for GitHub Pages
+  base: '/RMB-CV/', // The base URL for GitHub Pages
   plugins: [react()],
 });
